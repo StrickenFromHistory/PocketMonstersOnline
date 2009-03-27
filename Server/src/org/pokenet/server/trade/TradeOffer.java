74ca6da9e61a4ad1c2d46f47d9a247010632bca6
felty.wos <felty.wos@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-03-27 00:21:30 +0000

    Left out editted files and commented out then broken parts of these added files to avoid angering Gyarados. It will all almost certainly be scrapped, but its worse than nothing, right?

---
+++ Server/src/org/pokenet/server/trade/TradeOffer.java
@@ -0,0 +1,9 @@
+package org.pokenet.server.trade;
+
+import java.util.Vector;
+
+import org.pokenet.server.battle.Pokemon;
+
+public class TradeOffer extends Vector<Tradeable> {
+
+}
---
+++ Server/src/org/pokenet/server/trade/Tradeable.java
@@ -0,0 +1,9 @@
+package org.pokenet.server.trade;
+
+/**
+ * Provides an interface for all objects that can be traded
+ * @author felty.wos
+ */
+public interface Tradeable {
+
+}
