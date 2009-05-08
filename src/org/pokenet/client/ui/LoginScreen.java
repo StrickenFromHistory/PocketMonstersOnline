df3e0ff703b287f280c7a31aace78c7a8686c365
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-05-08 04:27:22 +0000

    Fixes broken input on login

--- src/org/pokenet/client/ui/LoginScreen.java
+++ src/org/pokenet/client/ui/LoginScreen.java
@@ -189,7 +189,7 @@ public class LoginScreen extends Window {
 	 * @return
 	 */
 	public void enterKeyDefault() {
-		if (!this.containsChild(m_select) && !this.containsChild(m_lang)){
+		if (!m_select.isVisible() && !m_lang.isVisible()){
 			if(m_select.isVisible()){
 				m_select.goServer();
 			}else{
