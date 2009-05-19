b4fcf1cae3a10e2d996432fc692da32b1b2f1262
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-05-19 03:39:37 +0000

    Bug fix, some code cleanup

--- src/org/pokenet/client/backend/BattleManager.java
+++ src/org/pokenet/client/backend/BattleManager.java
@@ -85,6 +85,7 @@ public class BattleManager {
 		m_timeLine.stop();
 		m_timeLine = null;
 		GameClient.getInstance().getDisplay().remove(m_battle);
+		while (GameClient.getInstance().getDisplay().containsChild(m_battle));
 		m_battle = null;
 		GameClient.changeTrack("newbark");
 	}
@@ -118,99 +119,16 @@ public class BattleManager {
 	 * Updates the pokemon pane
 	 */
 	public void updatePokePane() {
-		m_battle.pokeBtn1.setText(m_ourPokes[0].getName());
-		m_battle.info1.setText("Lv: " + m_ourPokes[0].getLevel() + " HP:"
-				+ m_ourPokes[0].getCurHP() + "/" + m_ourPokes[0].getMaxHP());
-		if (m_ourPokes[0].getCurHP() <= 0)
-			m_battle.pokeBtn1.setEnabled(false);
-		else
-			m_battle.pokeBtn1.setEnabled(true);
-
-		try {
-			m_battle.pokeBtn2.setText(m_ourPokes[1].getName());
-			m_battle.info2
-					.setText("Lv: " + m_ourPokes[1].getLevel() + " HP:"
-							+ m_ourPokes[1].getCurHP() + "/"
-							+ m_ourPokes[1].getMaxHP());
-			if (m_ourPokes[1].getCurHP() <= 0)
-				m_battle.pokeBtn2.setEnabled(false);
-			else
-				m_battle.pokeBtn2.setEnabled(true);
-		} catch (NullPointerException e) {
-			m_battle.pokeBtn2.setEnabled(false);
-		}
-
-		try {
-			m_battle.pokeBtn3.setText(m_ourPokes[2].getName());
-			m_battle.info3
-					.setText("Lv: " + m_ourPokes[2].getLevel() + " HP:"
-							+ m_ourPokes[2].getCurHP() + "/"
-							+ m_ourPokes[2].getMaxHP());
-			if (m_ourPokes[2].getCurHP() <= 0)
-				m_battle.pokeBtn3.setEnabled(false);
-			else
-				m_battle.pokeBtn3.setEnabled(true);
-		} catch (NullPointerException e) {
-			m_battle.pokeBtn3.setEnabled(false);
-		}
-
-		try {
-			m_battle.pokeBtn4.setText(m_ourPokes[3].getName());
-			m_battle.info4
-					.setText("Lv: " + m_ourPokes[3].getLevel() + " HP:"
-							+ m_ourPokes[3].getCurHP() + "/"
-							+ m_ourPokes[3].getMaxHP());
-			if (m_ourPokes[3].getCurHP() <= 0)
-				m_battle.pokeBtn4.setEnabled(false);
-			else
-				m_battle.pokeBtn4.setEnabled(true);
-		} catch (NullPointerException e) {
-			m_battle.pokeBtn4.setEnabled(false);
-		}
-		try {
-			m_battle.pokeBtn5.setText(m_ourPokes[4].getName());
-			m_battle.info5
-					.setText("Lv: " + m_ourPokes[4].getLevel() + " HP:"
-							+ m_ourPokes[4].getCurHP() + "/"
-							+ m_ourPokes[4].getMaxHP());
-			if (m_ourPokes[4].getCurHP() <= 0)
-				m_battle.pokeBtn5.setEnabled(false);
-			else
-				m_battle.pokeBtn5.setEnabled(true);
-		} catch (NullPointerException e) {
-			m_battle.pokeBtn5.setEnabled(false);
-		}
-		try {
-			m_battle.pokeBtn6.setText(m_ourPokes[5].getName());
-			m_battle.info6
-					.setText("Lv: " + m_ourPokes[5].getLevel() + " HP:"
-							+ m_ourPokes[5].getCurHP() + "/"
-							+ m_ourPokes[5].getMaxHP());
-			if (m_ourPokes[5].getCurHP() <= 0)
-				m_battle.pokeBtn6.setEnabled(false);
-			else
-				m_battle.pokeBtn6.setEnabled(true);
-		} catch (NullPointerException e) {
-			m_battle.pokeBtn6.setEnabled(false);
-		}
-
-	}
-
-	/**
-	 * Switches a pokemon on the field
-	 * 
-	 * @param trainerIndex
-	 * @param pokeIndex
-	 */
-	public void switchPoke(int trainerIndex,
-			int pokeIndex,
-			boolean isForced) {
-		if (trainerIndex == 0) {
-			// TODO: Draw our poke
-			updateMoves(pokeIndex);
-			m_battle.showPokePane(isForced);
-		} else {
-			// TODO: Draw enemy poke
+		for (int i = 0; i < 6; i++) {
+			try{
+				m_battle.m_pokeButtons.get(i).setText(m_ourPokes[0].getName());
+				m_battle.m_pokeInfo.get(i).setText("Lv: " + m_ourPokes[0].getLevel() + " HP:"
+						+ m_ourPokes[0].getCurHP() + "/" + m_ourPokes[0].getMaxHP());
+				if (m_ourPokes[0].getCurHP() <= 0)
+					m_battle.m_pokeButtons.get(i).setEnabled(false);
+				else
+					m_battle.m_pokeButtons.get(i).setEnabled(true);
+			} catch (Exception e) {}
 		}
 	}
 
