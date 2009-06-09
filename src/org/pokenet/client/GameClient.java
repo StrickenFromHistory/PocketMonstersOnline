4b990f84a692076da4d879addfaf14d82873a334
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-06-09 05:37:10 +0000

    Fixes music issue

--- src/org/pokenet/client/GameClient.java
+++ src/org/pokenet/client/GameClient.java
@@ -204,7 +204,6 @@ public class GameClient extends BasicGame {
 		 */
 		if(m_isNewMap && m_loading.isVisible()) {
 			m_mapMatrix.loadMaps(m_mapX, m_mapY, gc.getGraphics());
-			m_soundPlayer.setTrackByLocation(m_mapMatrix.getCurrentMap().getName());
 			while(m_ourPlayer == null);
 			m_mapMatrix.getCurrentMap().setXOffset(400 - m_ourPlayer.getX(), false);
 			m_mapMatrix.getCurrentMap().setYOffset(300 - m_ourPlayer.getY(), false);
@@ -648,6 +647,7 @@ public class GameClient extends BasicGame {
 		m_isNewMap = true;
 		m_loading.setVisible(true);
 		m_ui.getReqWindow().clearOffers();
+		m_soundPlayer.setTrackByLocation(m_mapMatrix.getMapName(x, y));
 	}
 	
 	/**
