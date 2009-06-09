8ec6df8d45e5805a530417fb7445c8be1c0ed358
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-06-09 03:44:29 +0000

    Threaded music loading, music won't start till all songs loaded however. Prevents long-ass startup times.

--- Client/src/org/pokenet/client/backend/SoundManager.java
+++ Client/src/org/pokenet/client/backend/SoundManager.java
@@ -14,8 +14,9 @@ import org.newdawn.slick.SlickException;
  */
 public class SoundManager extends Thread{
 	private HashMap<String, Music> m_files;
+	private HashMap<String, String> m_fileList;
 	protected String m_trackName;
-	private boolean m_muted = false, m_trackChanged = true, m_isRunning = false;
+	private boolean m_muted = false, m_tracksLoaded = false, m_trackChanged = true, m_isRunning = false;
 
 	private final String m_audioPath = "res/music/";
 
@@ -23,26 +24,24 @@ public class SoundManager extends Thread{
 	 * Default Constructor
 	 */
 	public SoundManager() {
+		m_files = new HashMap<String, Music>();
 		loadFileList();
 	}
 	
 	/**
-	 * Loads the files
+	 * Loads the file list
 	 */
 	private void loadFileList() {
 		try {
 			Scanner reader = new Scanner(new File(m_audioPath+"index.txt"));
-			m_files = new HashMap<String, Music>();
+			m_fileList = new HashMap<String, String>();
 
 			String f = null;
 			while (reader.hasNext()) {
 				f = reader.nextLine();
 				if (f.charAt(1) != '*'){
-					System.out.println(f);
 					String[] addFile = f.split(":", 2);
-					try {
-						m_files.put(addFile[0], new Music(m_audioPath + addFile[1]));
-					} catch (Exception e) {e.printStackTrace();}
+					m_fileList.put(addFile[0], addFile[1]);
 				}
 			}
 		} catch (Exception e) {
@@ -52,18 +51,34 @@ public class SoundManager extends Thread{
 	}
 	
 	/**
+	 * Loads the files
+	 */
+	private void loadFiles() {
+		for (String key : m_fileList.keySet()){
+			try {
+				m_files.put(key, new Music(m_audioPath + m_fileList.get(key)));
+				System.out.println("Loaded " + m_fileList.get(key));
+			} catch (Exception e) {e.printStackTrace();}
+		}
+		m_tracksLoaded = true;
+	}
+	
+	/**
 	 * Called by m_thread.start(). Loops through all players calling PlayerChar.move() if the player requested to be moved.
 	 */
 	@Override
 	public void run() {
 		while (m_isRunning){
+			while (!m_tracksLoaded) {
+				loadFiles();
+			}
 			if (m_trackChanged){
 				try{
 					m_trackChanged = false;
-					System.out.println("Playing: " + m_files.get(m_trackName));
+					System.out.println("Playing: " + m_fileList.get(m_trackName));
 					m_files.get(m_trackName).loop();
 				} catch (Exception e){
-					System.err.println("Failed to load " +  m_files.get(m_trackName));
+					System.err.println("Failed to load " +  m_fileList.get(m_trackName));
 					m_trackChanged = false;
 				}
 			}
