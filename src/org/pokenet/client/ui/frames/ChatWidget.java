fa8f6057738ca3e12366d9454f9a7d102fab212d
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-06-13 04:34:55 +0000

    Final chat commit, fixes one little tiny biny line rendering issue.

--- src/org/pokenet/client/ui/frames/ChatWidget.java
+++ src/org/pokenet/client/ui/frames/ChatWidget.java
@@ -67,7 +67,7 @@ class ChatWidget extends Container{
 	public void addLine() {
 		wrap();
 		if (m_wrappedText.size() - m_maxLines >= 0)
-			m_scrollIndex = m_wrappedText.size() - m_maxLines;
+			m_scrollIndex = m_wrappedText.size() - m_maxLines - 1;
 		else
 			m_scrollIndex = -1;
 		scroll(1);
