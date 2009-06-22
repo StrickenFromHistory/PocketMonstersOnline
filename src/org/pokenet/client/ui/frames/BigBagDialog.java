a0d810dbca368f31fd6308b6ecf233c68afc1a39
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-06-22 09:54:48 +0000

    BagDialog shows images again, BigBag is getting there

--- src/org/pokenet/client/ui/frames/BigBagDialog.java
+++ src/org/pokenet/client/ui/frames/BigBagDialog.java
@@ -1,7 +1,7 @@
 package org.pokenet.client.ui.frames;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import mdes.slick.sui.Button;
 import mdes.slick.sui.Frame;
@@ -12,9 +12,9 @@ import mdes.slick.sui.event.ActionListener;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.GUIContext;
 import org.newdawn.slick.loading.LoadingList;
 import org.pokenet.client.GameClient;
-import org.pokenet.client.backend.entity.Item;
 import org.pokenet.client.backend.entity.PlayerItem;
 import org.pokenet.client.ui.base.ImageButton;
 
@@ -25,44 +25,95 @@ import org.pokenet.client.ui.base.ImageButton;
  */
 public class BigBagDialog extends Frame {
 	private ImageButton[] m_categoryButtons;
-	
-	List<Item> m_items;
+	private ArrayList<Button> m_itemBtns = new ArrayList<Button>();
+	private ArrayList<Label> m_stockLabels = new ArrayList<Label>();
 	private Button m_cancel;
-	// string being the item name and integer being item quantity
-//	private List<Integer> m_merch;
-
+	
+	private HashMap<Integer, ArrayList<PlayerItem>> m_items = new HashMap<Integer, ArrayList<PlayerItem>>();
+	private HashMap<Integer, Integer> m_scrollIndex = new HashMap<Integer,Integer>();
+	private int m_curCategory = 0;
+	boolean m_update = false;
+	
 	public BigBagDialog() {
 		getContentPane().setX(getContentPane().getX() - 1);
 		getContentPane().setY(getContentPane().getY() + 1);
-//		m_merch = merch;
-//		packetGen = out;
 		setCenter();
 		initGUI();
+
+		// Load the player's items and sort them by category
+		for (PlayerItem i : GameClient.getInstance().getOurPlayer().getItems()){
+			System.out.println("Item: " + i.getItem().getName());
+			System.out.println(i.getItem().getCategory());
+			//Field items
+			if (i.getItem().getCategory().equalsIgnoreCase("Field")){
+				m_items.get(0).add(i);
+				System.out.println("Added to field! " + m_items.get(0).size());
+			}
+			//Potions and medicine
+			else if (i.getItem().getCategory().equalsIgnoreCase("Potions") ||
+					i.getItem().getCategory().equalsIgnoreCase("Medicine")){
+				m_items.get(1).add(i);
+				System.out.println("Added to potions! " + m_items.get(1).size());
+			}
+			// Berries and food
+			else if (i.getItem().getCategory().equalsIgnoreCase("Food")){
+				m_items.get(2).add(i);	
+				System.out.println("Added to Berries! " + m_items.get(2).size());
+			}
+			// Pokeballs
+			else if (i.getItem().getCategory().equalsIgnoreCase("Pokeball")){
+				m_items.get(3).add(i);
+				System.out.println("Added to pokeballs! " + m_items.get(3).size());
+			}
+			//TMs
+			else if (i.getItem().getCategory().equalsIgnoreCase("TM") ){
+				m_items.get(4).add(i);
+				System.out.println("Added to TMs! " + m_items.get(4).size());
+			}
+		}
+		m_update = true;
 	}
 	
+	/**
+	 * Initializes the interface
+	 */
 	public void initGUI() {
 		m_categoryButtons = new ImageButton[5];
 		
-		
-		
-		for(int i = 0; i < m_categoryButtons.length;i++){
+		for(int i = 0; i < m_categoryButtons.length; i++){
+			final int j = i;
 			try{
-				Image bagcat = new Image("res/ui/bag/bag_normal.png");
-				Image berriescat = new Image("res/ui/bag/berries_normal.png");
-				Image pokecat = new Image("res/ui/bag/pokeballs_normal.png");
-				Image potioncat = new Image("res/ui/bag/potions_normal.png");
-				Image tmscat = new Image("res/ui/bag/tms_normal.png");
+				Image[] bagcat = new Image[] {new Image("res/ui/bag/bag_normal.png"),
+						new Image("res/ui/bag/bag_hover.png"), new Image("res/ui/bag/bag_pressed.png")};
+				Image[] potioncat = new Image[] {new Image("res/ui/bag/potions_normal.png"),
+						new Image("res/ui/bag/potions_hover.png"), new Image("res/ui/bag/potions_pressed.png")};
+				Image[] berriescat = new Image[] {new Image("res/ui/bag/berries_normal.png"),
+						new Image("res/ui/bag/berries_hover.png"), new Image("res/ui/bag/berries_pressed.png")};
+				Image[] pokecat = new Image[] {new Image("res/ui/bag/pokeballs_normal.png"),
+						new Image("res/ui/bag/pokeballs_hover.png"), new Image("res/ui/bag/pokeballs_pressed.png")};
+				Image[] tmscat = new Image[] {new Image("res/ui/bag/tms_normal.png"),
+						new Image("res/ui/bag/tms_hover.png"), new Image("res/ui/bag/tms_pressed.png")};
 				
-				if(i==0)
-					m_categoryButtons[i] = new ImageButton(bagcat, bagcat, bagcat);
-				else if(i==1)
-					m_categoryButtons[i] = new ImageButton(potioncat, potioncat, potioncat);
-				else if(i==2)
-					m_categoryButtons[i] = new ImageButton(berriescat, berriescat, berriescat);
-				else if(i==3)
-					m_categoryButtons[i] = new ImageButton(pokecat, pokecat, pokecat);
-				else if(i==4)
-					m_categoryButtons[i] = new ImageButton(tmscat, tmscat, tmscat);
+				switch (i){
+				case 0:
+					m_categoryButtons[i] = new ImageButton(bagcat[0], bagcat[1], bagcat[2]);
+					break;
+				case 1:
+					m_categoryButtons[i] = new ImageButton(potioncat[0], potioncat[1], potioncat[2]);
+					break;
+				case 2:
+					m_categoryButtons[i] = new ImageButton(berriescat[0], berriescat[1], berriescat[2]);
+					break;
+				case 3:
+					m_categoryButtons[i] = new ImageButton(pokecat[0], pokecat[1], pokecat[2]);
+					break;
+				case 4:
+					m_categoryButtons[i] = new ImageButton(tmscat[0], tmscat[1], tmscat[2]);
+					break;
+				}
+				
+				m_items.put(i, new ArrayList<PlayerItem>());
+				m_scrollIndex.put(i, 0);
 			}catch(Exception e){
 				e.printStackTrace();
 			}
@@ -70,125 +121,138 @@ public class BigBagDialog extends Frame {
 			if(i==0)
 				m_categoryButtons[i].setLocation(80,10);
 			else
-				m_categoryButtons[i].setLocation(m_categoryButtons[i-1].getLocation().x+65, 10);
+				m_categoryButtons[i].setLocation(m_categoryButtons[i-1].getX() + 65, 10);
 			m_categoryButtons[i].setFont(GameClient.getFontLarge());
 			m_categoryButtons[i].setOpaque(false);
 			m_categoryButtons[i].addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-//					categoryClicked(0);
+					m_curCategory = j;
+					m_update = true;
 				}
 			});
 			getContentPane().add(m_categoryButtons[i]);
 		}
 		
+		// Bag Image
 		Label bagicon = new Label("");
 		bagicon.setSize(40,40);
+		
 		LoadingList.setDeferredLoading(true);
 		try {
 			bagicon.setImage(new Image("res/ui/bag/front.png"));
 		} catch (SlickException e1) {}
 		LoadingList.setDeferredLoading(false);
+		
 		bagicon.setLocation(18,0);
 		bagicon.setFont(GameClient.getFontLarge());
 		getContentPane().add(bagicon);
 		
-		Button leftbutton = new Button("<");
-		leftbutton.setSize(20,40);
-		leftbutton.setLocation(15,95);
-		leftbutton.setFont(GameClient.getFontLarge());
-		getContentPane().add(leftbutton);
+		// Scrolling Button LEFT
+		Button leftButton = new Button("<");
+		leftButton.setSize(20,40);
+		leftButton.setLocation(15,95);
+		leftButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int i = m_scrollIndex.get(m_curCategory) - 1;
+				m_scrollIndex.remove(m_curCategory);
+				m_scrollIndex.put(m_curCategory, i);
+				m_update = true;
+			}
+		});
+		getContentPane().add(leftButton);
 		
-		Button item0 = new Button("id0");
-		item0.setSize(60,60);
-		item0.setLocation(50,85);
-		item0.setFont(GameClient.getFontLarge());
-		getContentPane().add(item0);
-		
-		Label potionicon = new Label("  x20");
-		potionicon.setSize(40,40);
-		potionicon.setLocation(55,135);
-		potionicon.setFont(GameClient.getFontLarge());
-		getContentPane().add(potionicon);
-		
-		
-		
-		Button item1 = new Button("id1");
-		item1.setSize(60,60);
-		item1.setLocation(130,85);
-		item1.setFont(GameClient.getFontLarge());
-		getContentPane().add(item1);
-		
-		Label icon2 = new Label("  x15");
-		icon2.setSize(40,40);
-		icon2.setLocation(135,135);
-		icon2.setFont(GameClient.getFontLarge());
-		getContentPane().add(icon2);
-		
-		Button item2 = new Button("id2");
-		item2.setSize(60,60);
-		item2.setLocation(210,85);
-		item2.setFont(GameClient.getFontLarge());
-		getContentPane().add(item2);
-		
-		Label icon3 = new Label("  x10");
-		icon3.setSize(40,40);
-		icon3.setLocation(215,135);
-		icon3.setFont(GameClient.getFontLarge());
-		getContentPane().add(icon3);
-		
-		Button item3 = new Button("id3");
-		item3.setSize(60,60);
-		item3.setLocation(290,85);
-		item3.setFont(GameClient.getFontLarge());
-		getContentPane().add(item3);
-		
-		Label icon4 = new Label("  x0");
-		icon4.setSize(40,40);
-		icon4.setLocation(290,135);
-		icon4.setFont(GameClient.getFontLarge());
-		getContentPane().add(icon4);
+		// Item Buttons and Stock Labels
+		for (int i = 0; i < 4; i++){
+			// Starts the item buttons
+			Button item = new Button("id" + i);
+			item.setSize(60,60);
+			item.setLocation(50 + (80 * i), 85);
+			item.setFont(GameClient.getFontLarge());
+			m_itemBtns.add(item);
+			getContentPane().add(item);
+
+			// Starts the item labels
+			Label stock = new Label("x0");
+			stock.setSize(60,40);
+			stock.setLocation(50 + (80 * i),135);
+			stock.setHorizontalAlignment(Label.CENTER_ALIGNMENT);
+			stock.setFont(GameClient.getFontLarge());
+			stock.setForeground(Color.white);
+			m_stockLabels.add(stock);
+			getContentPane().add(stock);
+		}
 			
-		Button rightbutton = new Button(">");
-		rightbutton.setSize(20,40);
-		rightbutton.setLocation(365,95);
-		rightbutton.setFont(GameClient.getFontLarge());
-		getContentPane().add(rightbutton);
+		// Scrolling Button Right
+		Button rightButton = new Button(">");
+		rightButton.setSize(20,40);
+		rightButton.setLocation(365,95);
+		rightButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int i = m_scrollIndex.get(m_curCategory) + 1;
+				m_scrollIndex.remove(m_curCategory);
+				m_scrollIndex.put(m_curCategory, i);
+				m_update = true;
+			}
+		});
+		getContentPane().add(rightButton);
 		
+		// Close Button
 		m_cancel = new Button("Close");
 		m_cancel.setSize(400,32);
 		m_cancel.setLocation(0,195);
-		m_cancel.setFont(GameClient.getFontLarge());
 		m_cancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				cancelled();
+				closeBag();
 			}
 		});
 		getContentPane().add(m_cancel);
 		
-		this.getResizer().setVisible(false);
+		// Frame properties
+		getResizer().setVisible(false);
 		getCloseButton().addActionListener(
 				new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						cancelled();
+						closeBag();
 					}
 				});
-		this.setBackground(new Color(0, 0, 0, 75));
+		setBackground(new Color(0, 0, 0, 75));
 		setTitle("Bag");
 		setResizable(false);
 		setHeight(250);
-		setWidth(m_categoryButtons.length*80);
-		pack();
+		setWidth(m_categoryButtons.length * 80);
 		setVisible(true);
 		setCenter();
 	}
 
-	public void cancelled() {
-//		packetGen.write("Sf");
+	/**
+	 * Closes the bag
+	 */
+	public void closeBag() {
 		setVisible(false);
 		GameClient.getInstance().getDisplay().remove(this);
 	}
-	public void pack() {
-		
+	
+	@Override
+	public void update(GUIContext gc, int delta){
+		super.update(gc, delta);
+		if (m_update){
+			m_update = false;
+			for (int i = 0; i < 5; i++){
+				if (m_items.get(m_curCategory).size() > (m_scrollIndex.get(m_curCategory) + i)){
+					try{
+						System.out.println(m_items.get(m_curCategory).get(m_scrollIndex.get(
+								m_curCategory + i)).getItem().getName());
+						m_itemBtns.get(i).setImage(m_items.get(m_curCategory).get(m_scrollIndex.get(
+								m_curCategory + i)).getBagImage());
+					} catch (Exception e) {e.printStackTrace();}
+				} else {
+					System.out.println("Item list length is shorter than " + 
+							(m_scrollIndex.get(m_curCategory) + i));
+					System.out.println(m_curCategory + " " + m_items.get(m_curCategory).size());
+					m_itemBtns.get(i).setImage(null);
+				}
+			}
+		}
 	}
 	
 	/**
