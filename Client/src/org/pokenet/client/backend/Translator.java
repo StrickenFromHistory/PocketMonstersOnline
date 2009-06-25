f137ca4aa00dfec37af3250086a273693af068c3
shadowkanji@hotmail.com <shadowkanji@hotmail.com@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-06-25 00:34:00 +0000

    Fixes JNLP issues! YAY!

--- Client/src/org/pokenet/client/backend/Translator.java
+++ Client/src/org/pokenet/client/backend/Translator.java
@@ -1,48 +1,77 @@
 package org.pokenet.client.backend;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.pokenet.client.GameClient;
 
 public class Translator {
+	private static Translator m_instance;
 	
-	@SuppressWarnings("deprecation")
-	public static List<String> translate(String filename){
-		BufferedReader reader;
+	/**
+	 * Returns a list of translated text
+	 * @param filename
+	 * @return
+	 */
+	public List<String> translateText(String filename) {
 		List<String> translated = new ArrayList<String>();
-		String f;
 		try {
-			reader = FileLoader.loadTextFile("/res/language/" + GameClient.getLanguage() + "/UI/" + filename + ".txt");
-			while ((f = reader.readLine()) != null) {
-				translated.add(f.replaceAll("/n", "\n"));
-				if(translated.size()==0){
+			String path = "/res/language/" + GameClient.getLanguage() + "UI/" + filename + ".txt";
+			System.out.println("TRANSLATE PATH: " + path);
+			InputStream in = getClass().getResourceAsStream(path);
+			if(in != null) {
+				BufferedReader f = new BufferedReader(new InputStreamReader(in));
+				Scanner reader = new Scanner(f);
+				while(reader.hasNextLine()) {
+					translated.add(reader.nextLine().replaceAll("/n", "\n"));
+				}
+				/*if(translated.size()==0){
 					FileInputStream fis = new FileInputStream(f);
 					BufferedInputStream bis = new BufferedInputStream(fis);
 					DataInputStream dis = new DataInputStream(bis);
-					while (dis.available() != 0) {
-						// this statement reads the line from the file
-						translated.add(dis.readLine());
+					 while (dis.available() != 0) {
+						 // this statement reads the line from the file
+						 translated.add(dis.readLine());
+					 }
+					 fis.close();
+					 bis.close();
+					 dis.close();
+				}*/
+			}else{ //In case of emergencies, load english!
+				try{
+					in = getClass().getResourceAsStream("/res/language/english/UI/" + filename + ".txt");
+					BufferedReader f = new BufferedReader(new InputStreamReader(in));
+					Scanner reader = new Scanner(f);
+					while(reader.hasNextLine()) {
+						translated.add(reader.nextLine().replaceAll("/n", "\n"));
 					}
-					fis.close();
-					bis.close();
-					dis.close();
+				}catch(Exception e){
+					translated.add("/n"); //If there's no english, display default line. 
 				}
+
 			}
-		} catch (Exception e) { //In case of emergencies, load english!
-			try{
-				reader = FileLoader.loadTextFile("/res/language/english/UI/" + filename + ".txt");
-				while((f = reader.readLine()) != null) {
-					translated.add(f.replaceAll("/n", "\n"));
-				}
-			}catch(Exception e2){
-				translated.add("/n"); //If there's no english, display default line. 
-			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return translated;
+	}
+	
+	/**
+	 * Returns the instance of translator
+	 * @return
+	 */
+	public static Translator getInstance() {
+		if(m_instance == null)
+			m_instance = new Translator();
+		return m_instance;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static List<String> translate(String filename){
+		return Translator.getInstance().translateText(filename);
 	}
 }
