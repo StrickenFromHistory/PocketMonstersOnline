a13226b19d8c320988b18f99b72ea007418e0c35
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-06-26 14:13:31 +0000

    Removes unnecessarty import

--- Server/src/org/pokenet/server/battle/Pokemon.java
+++ Server/src/org/pokenet/server/battle/Pokemon.java
@@ -41,7 +41,6 @@ import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import org.pokenet.server.GameServer;
 import org.pokenet.server.backend.entity.PlayerChar;
 import org.pokenet.server.battle.mechanics.BattleMechanics;
 import org.pokenet.server.battle.mechanics.ModData;
