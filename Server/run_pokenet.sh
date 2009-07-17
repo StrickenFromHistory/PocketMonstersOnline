c010c1f9c61525df45d24ec6553dd15eaa5f2bf8
nushio <nushio@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-07-17 17:52:17 +0000

    Ok, supposedly, this file really fixes the issue. :S

--- Server/run_pokenet.sh
+++ Server/run_pokenet.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 pokerun=`ps -efa | egrep java * Pokenet.jar | grep -v grep`
-	if [ "$pokerun" = "" ]; then
+        if [ "$pokerun" = "" ]; then
         echo "Starting Pokenet Server..." 
-    	java -Xmx956m -jar /home/pokenet/Server/Pokenet.jar -s low -p 100 --nogui --autorun > pokenet.log &
+        java -Xmx956m -jar /home/pokenet/Server/Pokenet.jar -s low -p 100 --nogui --autorun > pokenet.log &
         echo "Pokenet started!!!"
-	else
+        else
         echo "Pokenet is already running!"
     fi
+
