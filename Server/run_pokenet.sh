ff4e11a073bcf53249cfed43217b5b12153c6710
nushio <nushio@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-07-17 17:59:39 +0000

    Final fix to the dmaned sh script

--- Server/run_pokenet.sh
+++ Server/run_pokenet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pokerun=`ps -efa | egrep java * Pokenet.jar | grep -v grep`
+pokerun=`ps U pokenet | grep -v grep | grep Pokenet.jar`
         if [ "$pokerun" = "" ]; then
         echo "Starting Pokenet Server..." 
         java -Xmx956m -jar /home/pokenet/Server/Pokenet.jar -s low -p 100 --nogui --autorun > pokenet.log &
