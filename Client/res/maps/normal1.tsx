4e21fae0ab1e117e84f0f0f0a49fb3d9e6bca4f2
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-07-17 03:09:12 +0000

    Return of the grass overlay

--- Client/res/maps/normal1.tsx
+++ Client/res/maps/normal1.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
  <tileset name="1" firstgid="1" tilewidth="16" tileheight="16">
   <image source="normal1.png" trans="000000"/>
+  <tile id="32">
+    <properties>
+     <property name="Grass" value="true"/>
+    </properties>
+  </tile>
  </tileset>
