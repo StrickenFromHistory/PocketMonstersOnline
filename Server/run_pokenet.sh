c0f3db27fb2c91ee713b277e4df11d1290f7e882
nushio <nushio@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-07-17 17:42:40 +0000

    Saves output to pokenet.log

--- Server/run_pokenet.sh
+++ Server/run_pokenet.sh
@@ -2,7 +2,7 @@
 pokerun=`ps -efa | egrep java * Pokenet.jar | grep -v grep`
 	if [ "$pokerun" = "" ]; then
         echo "Starting Pokenet Server..." 
-    	java -Xmx956m -jar /home/pokenet/Server/Pokenet.jar -s low -p 100 --nogui --autorun &
+    	java -Xmx956m -jar /home/pokenet/Server/Pokenet.jar -s low -p 100 --nogui --autorun > pokenet.log &
         echo "Pokenet started!!!"
 	else
         echo "Pokenet is already running!"
