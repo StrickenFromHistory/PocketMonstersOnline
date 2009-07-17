40310d16741f64287b389fe0fbb2e7194a334fdd
nushio <nushio@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-07-17 17:42:20 +0000

    Updated RunPokenet sh Commits Pokenet.jar, which is the compiled Jar. This should help make server admin life easier.

--- Server/run_pokenet.sh
+++ Server/run_pokenet.sh
@@ -2,7 +2,7 @@
 pokerun=`ps -efa | egrep java * Pokenet.jar | grep -v grep`
 	if [ "$pokerun" = "" ]; then
         echo "Starting Pokenet Server..." 
-    	java -Xmx2056m -jar /home/pokenet/Server/Pokenet.jar -s low -p 500 --nogui --autorun &
+    	java -Xmx956m -jar /home/pokenet/Server/Pokenet.jar -s low -p 100 --nogui --autorun &
         echo "Pokenet started!!!"
 	else
         echo "Pokenet is already running!"
