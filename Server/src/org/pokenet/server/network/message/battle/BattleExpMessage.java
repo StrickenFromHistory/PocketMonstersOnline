67be33a3f1beaa4ff9213ca0b8299e4cdbe45d8a
apoliak@gmail.com <apoliak@gmail.com@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-08-14 06:34:15 +0000

    added remaining experience parameter

--- Server/src/org/pokenet/server/network/message/battle/BattleExpMessage.java
+++ Server/src/org/pokenet/server/network/message/battle/BattleExpMessage.java
@@ -1,19 +1,21 @@
+
 package org.pokenet.server.network.message.battle;
 
 import org.pokenet.server.network.message.PokenetMessage;
 
 /**
  * Exp gain during battle
+ * 
  * @author shadowkanji
- *
  */
 public class BattleExpMessage extends PokenetMessage {
-	/**
-	 * Constructor
-	 * @param pokeName
-	 * @param exp
-	 */
-	public BattleExpMessage(String pokeName, double exp) {
-		m_message = "b." + pokeName + "," + exp;
-	}
+  /**
+   * Constructor
+   * 
+   * @param pokeName
+   * @param exp
+   */
+  public BattleExpMessage(String pokeName, double exp, double rem) {
+    m_message = "b." + pokeName + "," + exp + "," + rem;
+  }
 }
