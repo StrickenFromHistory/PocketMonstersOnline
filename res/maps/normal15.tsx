e75e4fed3a2255709e57d09da7a3e345f139ffe0
drawig <drawig@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-08-21 22:05:11 +0000

    Added some tiles and fixed some old ones

--- res/maps/normal15.tsx
+++ res/maps/normal15.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="normal15" firstgid="9217" tilewidth="16" tileheight="16">
+<tileset name="Unbenannt" firstgid="3073" tilewidth="16" tileheight="16">
  <image source="normal15.png" trans="000000"/>
 </tileset>
