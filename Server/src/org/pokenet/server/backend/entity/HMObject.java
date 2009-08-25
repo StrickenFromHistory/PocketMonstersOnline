32cec1828c4b3a04b033a63464727efde1e1f557
javierbere <javierbere@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-08-25 04:50:01 +0000

    Initial serverside commit for HMObjects

--- Server/src/org/pokenet/server/backend/entity/HMObject.java
+++ Server/src/org/pokenet/server/backend/entity/HMObject.java
@@ -1,18 +1,53 @@
 
 package org.pokenet.server.backend.entity;
 
-public class HMObject extends Char {
-  public enum objectType {
-    ROCK_SMASH, CUT, DE_FOG, FLY, SURF, STRENGTH, ROCK_CLIMB, WATERFALL
-  };
 
-  private objectType m_HMType;
+public class HMObject extends NonPlayerChar {
+	public enum objectType {
+		ROCKSMASH_ROCK, CUT_TREE, STRENGTH_BOULDER, WHIRLPOOL
+	}
 
-  public objectType getType() {
-    return m_HMType;
-  }
+	private objectType m_HMType;
 
-  public void setType(objectType oT) {
-    m_HMType = oT;
-  }
+	public objectType getType() {
+		return m_HMType;
+	}
+
+	public void setType(objectType oT) {
+		m_HMType = oT;
+	}
+	
+	public int getNecessaryTrainerLevel(objectType oT) {
+		switch (oT) {
+		case ROCKSMASH_ROCK:
+			return 30;
+		case CUT_TREE:
+			return 15;
+		case STRENGTH_BOULDER:
+			return 35;
+		case WHIRLPOOL:
+			return 40;
+		}
+		return 0;
+	}
+	
+	@Override
+	public void talkToPlayer(PlayerChar p) {
+		// Handle event
+		if (p.getTrainingLevel() >= getNecessaryTrainerLevel(m_HMType)) {
+			switch (m_HMType){
+			case STRENGTH_BOULDER :
+				setNextMovement(p.getFacing());
+				break;
+			default :
+				getMap().removeChar(this);
+				// Launch a timer to readd the element?
+				break;
+			}
+		} else {
+			// This shouldn't happen unless the client is modified.
+			// Log the player's name on the output log
+			System.out.println(p.getName() + " has attempted to perform an illegal operation.");
+		}
+	}
 }
