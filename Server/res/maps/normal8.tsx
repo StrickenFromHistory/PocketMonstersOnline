f013dc299e08fa5d298ef045675922ea64ab64c9
drawig <drawig@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2009-09-05 19:54:09 +0000

    Removed HM objects which were mapped into the maps. Updated server to the newest maps and tilesets.

--- Server/res/maps/normal8.tsx
+++ Server/res/maps/normal8.tsx
@@ -1,4 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
  <tileset name="1" firstgid="1" tilewidth="16" tileheight="16">
   <image source="normal8.png" trans="000000"/>
+ <tile id="789">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="790">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="791">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="821">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="822">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="823">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="853">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="854">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
+ <tile id="855">
+  <properties>
+   <property name="Reflection" value="true"/>
+  </properties>
+ </tile>
  </tileset>
