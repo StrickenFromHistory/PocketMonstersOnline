42f9612a51cb7d541225c8f43858d36966faa495
Lt.Sego <Lt.Sego@c4dcdc68-ee3c-11dd-a1cf-5f4f5b310efa>
2010-03-19 03:40:40 +0000

    syncing

--- SVNClient/src/Pokenet.java
+++ SVNClient/src/Pokenet.java
@@ -15,6 +15,7 @@ import javax.swing.JTextArea;
 import org.tmatesoft.svn.cli.SVNCommandUtil;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
 import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
 import org.tmatesoft.svn.core.wc.ISVNOptions;
@@ -52,10 +53,10 @@ public class Pokenet extends JFrame  implements Runnable {
 		
 		scrollPane = new JScrollPane(outText);
 
+		new Thread(this).start();
 		
 		ImageIcon m_logo;
 		JLabel l = null;
-		System.out.println("5...");
 
 		try {
 			m_logo = new ImageIcon(new URL("http://pokedev.org/header.png"));
@@ -63,32 +64,15 @@ public class Pokenet extends JFrame  implements Runnable {
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		System.out.println("4...");
 
 		
 		l.setBounds(0,0,740,190);
 		outText.setBounds(0, 190, 740, 440-190);
 		scrollPane.setBounds(0,190,740,450-190);
-		System.out.println("3...");
-
-//		Insets insets = this.getInsets();
-//        Dimension size = l.getPreferredSize();
-//        l.setBounds(insets.left,insets.top,
-//                     size.width, size.height);
-//        size = outText.getPreferredSize();
-//        outText.setBounds(insets.left, 190 + insets.top,
-//                     size.width, size.height - 190);
-//		System.out.println("2...");
 
         this.add(l);
 		this.add(scrollPane);
-		System.out.println("Starting...");
 		this.setVisible(true);
-		
-//		FSRepositoryFactory.setup(); // for local access (file protocol). 
-//		SVNRepositoryFactoryImpl.setup(); // for svn(+ssh) protocol 
-		
-		new Thread(this).start();
 
 	}
 	
@@ -99,97 +83,84 @@ public class Pokenet extends JFrame  implements Runnable {
 		Pokenet c = new Pokenet();
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void run() {
-		/*
-		 * TODO:
-		 * make sure they have SVN...
-		 * if not, install it
-		 */
+
 		Process svn;
 		Thread t = null;
 //		String command;
 		
-			ISVNOptions options = SVNWCUtil.createDefaultOptions(true);
-		
-			 /* 
-		     * Creates an instance of SVNClientManager providing authentication 
-		     * information (name, password) and an options driver 
-		     */ 
-			SVNClientManager ourClientManager = SVNClientManager.newInstance(options); 
-		    SVNUpdateClient updateClient = ourClientManager.getUpdateClient(); 
+		DAVRepositoryFactory.setup();
+		ISVNOptions options = SVNWCUtil.createDefaultOptions(true);
+	
+		 /* 
+	     * Creates an instance of SVNClientManager providing authentication 
+	     * information (name, password) and an options driver 
+	     */ 
+		SVNClientManager ourClientManager = SVNClientManager.newInstance(options); 
+	    SVNUpdateClient updateClient = ourClientManager.getUpdateClient(); 
 
-		    /* 
-		     * sets externals not to be ignored during the checkout 
-		     */ 
-		    updateClient.setIgnoreExternals(false); 
+	    /* 
+	     * sets externals not to be ignored during the checkout 
+	     */ 
+	    updateClient.setIgnoreExternals(false); 
+		
+	    /* 
+	     * A url of a repository to check out 
+	     */ 
+	    SVNURL url = null;
+		try {
+			url = SVNURL.parseURIDecoded("http://" + SVN_URL);
+		} catch (SVNException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} 
+	    /* 
+	     * A revision to check out 
+	     */ 
+	    SVNRevision revision = SVNRevision.HEAD; 
+
+	    /* 
+	     * A revision for which you're sure that the url you specify is 
+	     * exactly what you need 
+	     */ 
+	    SVNRevision pegRevision = SVNRevision.HEAD; 
+
+	    /* 
+	     * Set whether a checkout is recursive or not 
+	     */ 
+	    boolean isRecursive = true; 
+
+	    /* 
+	     * A local path where a Working Copy will be ckecked out 
+	     */ 
+	    File destPath = new File(FOLDER_NAME); 
+
+	    /* 
+	     * returns the number of the revision at which the working copy is 
+	     */ 
+	    try {
 			
-		    /* 
-		     * A url of a repository to check out 
-		     */ 
-		    SVNURL url = null;
-			try {
-				url = SVNURL.parseURIDecoded("http://" + SVN_URL);
-			} catch (SVNException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			} 
-		    /* 
-		     * A revision to check out 
-		     */ 
-		    SVNRevision revision = SVNRevision.HEAD; 
+			boolean exists = destPath.exists();
+			if(!exists) {
+				this.outText.append("Installing...\n Please be patient while PokeNet is downloaded...\n");
+				System.out.println("Installing...");
+				updateClient.doCheckout(url, destPath, pegRevision, 
+                        revision, isRecursive);
 
-		    /* 
-		     * A revision for which you're sure that the url you specify is 
-		     * exactly what you need 
-		     */ 
-		    SVNRevision pegRevision = SVNRevision.HEAD; 
+			} else {
+				this.outText.append("Updating...\n");
+				System.out.println("Updating...\n");
+				updateClient.doUpdate(destPath, revision, isRecursive);
+			}
+	    } catch (SVNException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
 
-		    /* 
-		     * Set whether a checkout is recursive or not 
-		     */ 
-		    boolean isRecursive = true; 
 
-		    /* 
-		     * A local path where a Working Copy will be ckecked out 
-		     */ 
-		    File destPath = new File(FOLDER_NAME); 
-
-		    /* 
-		     * returns the number of the revision at which the working copy is 
-		     */ 
-		    try {
-				
-				boolean exists = destPath.exists();
-				if(!exists) {
-					this.outText.append("Installing...\n Please be patient while PokeNet is downloaded...\n");
-					System.out.println("Installing...");
-					updateClient.doCheckout(url, destPath, pegRevision, 
-                            revision, isRecursive);
-
-				} else {
-					this.outText.append("Updating...\n");
-					System.out.println("Updating...\n");
-					updateClient.doUpdate(destPath, revision, true);
-				}
-		    } catch (SVNException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} 
-
-	        
-	        
-			
-		
-		
-		  try { 
-
-		                
-		    } catch (Exception e) { 
-		        e.toString(); 
-		    }
-
-		
+	
 //		try {
 //			svn = Runtime.getRuntime().exec(command);
 //			StreamReader sr = new StreamReader(svn.getInputStream(), "", outText);
